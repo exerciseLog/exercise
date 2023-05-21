601c7e269bf00f3f2d9ff679492b77d6c6e31726
ChoiHyeonSu0 <choi1211012@gmail.com>
2023-05-21 20:03:05 +0900

    not my code delete commit

--- lib/table/memo.dart
+++
@@ -1,8 +0,0 @@
-import 'package:drift/drift.dart';
-
-class Memo extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get memo => text()();
-  DateTimeColumn get writeTime => dateTime()();
-  DateTimeColumn get modifyTime => dateTime()();
-}
